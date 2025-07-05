afa0157379e2ab535fc12e4b6541dcfc16e2a96d
Mark Harrison <mharrison6@sheffield.ac.uk>
2025-07-05 16:18:16 +0100

    MainWindow::parseXML parses and outputs the raw text content of the XML chat log

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -42,11 +42,50 @@ void MainWindow::handleOpenAction() {
     QString xmlData = file.readAll();
 
     // Pass the XML data to the parser to format it for display
-    loadXMLToBrowser(xmlData);
+    parseXML(xmlData);
 
 }
 
 void MainWindow::parseXML(QString data) {
+
+    int index = 0;
+
+    while (index < data.length()) {
+
+        // Get the index of the next start tag
+        int tagStart = data.indexOf('<', index);
+
+        // Check if there are no more tags left to parse
+        if (tagStart == -1) {
+            break;
+        }
+
+        // Get the index of the end of the tag
+        int tagEnd = data.indexOf('>', tagStart);
+
+        // // Retrieve the contents of the tag (unless it's a closing tag)
+        // if (data[tagStart + 1] != '/') {
+
+        //     int tagLength = tagEnd - (tagStart + 1);
+
+        //     QString tag = data.mid(tagStart + 1, tagLength);
+
+        //     ui->xmlViewer->append(tag);
+
+        // }
+
+        // Retrieve the text contents of the tag and trim it
+        int contentLength = data.indexOf('<', tagEnd) - (tagEnd + 1);
+        QString textContent = data.mid(tagEnd + 1, contentLength).trimmed();
+
+        // Output the
+        if (!textContent.isEmpty()) {
+            ui->xmlViewer->append(textContent + "\n");
+        }
+
+        index = tagEnd + 1;
+
+    }
 
 }
 
